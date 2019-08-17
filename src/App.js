0b88ed27fa949d19cda7dc735500bc618db9d1ce
Sahil Khokhar <sahilkhokhar505@gmail.com>
2019-08-17 23:38:37 +0530

    hooks and get posts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+  })
   return (
     <div className="container">
       <h1>My App</h1>
