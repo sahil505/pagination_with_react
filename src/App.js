d3a5887a62c9f0a29d2866bcfd1e2d8f8a1c6e7c
Sahil Khokhar <sahilkhokhar505@gmail.com>
2019-08-18 00:37:07 +0530

    DONE DEAL

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -25,11 +25,14 @@ const App = () => {
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
+  // Change Page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div className="container mt-5">
       <h1 className = 'text-primary mb-3'>My Blog</h1>
       <Posts posts={currentPosts} loading={loading}/>
-      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length}/>
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate}/>
     </div>
   );
 };
