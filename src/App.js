7b3211487a6eb5c666c72ae0e5bf872e0e30d859
Sahil Khokhar <sahilkhokhar505@gmail.com>
2019-08-18 00:07:24 +0530

    posts up on the page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import Posts from './components/posts';
 import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
@@ -14,16 +15,17 @@ function App() {
       const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
       setPosts(res.data);
       setLoading(false);
-    }
+    };
     fetchPosts();
   }, []);
 
   console.log(posts);
   return (
-    <div className="container">
-      <h1>My App</h1>
+    <div className="container mt-5">
+      <h1 className = 'text-primary mb-3'>My Blog</h1>
+      <Posts posts={posts} loading={loading}/>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,4 +1,4 @@
-import react from 'react';
+import React from 'react';
 
 const Posts = ({ posts, loading }) => {
   if (loading) {
