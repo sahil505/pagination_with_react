ae2ed81b6e7a1ab6bf100b27a7a2fe3556084880
Sahil Khokhar <sahilkhokhar505@gmail.com>
2019-08-18 00:01:48 +0530

    posts frontend

---
+++ src/posts.js
@@ -0,0 +1,18 @@
+import react from 'react';
+
+const Posts = ({ posts, loading }) => {
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+  return (
+    <ul className = 'list-group mb-4'>
+      {posts.map(posts => (
+        <li key={posts.id} className='list-group-item'>
+        {posts.title}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default Posts;
